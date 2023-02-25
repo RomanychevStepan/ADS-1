1f6ad6eee2fc9820da26bb5cf07b7b655363464f
RomanychevStepan <124895851+RomanychevStepan@users.noreply.github.com>
2023-02-25 14:44:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,25 +3,25 @@
 #include "alg.h"
 
 
-bool checkPrime(uint64_t value) { 
-  if (value <= 1) { 
+bool checkPrime(uint64_t value) {
+  if (value <= 1) {
     return false; 
-  } 
-  else { 
-    for (uint64_t i = 2; i <= value / 2; i++) { 
-      if (value % i == 0) { 
+  }
+  else {
+    for (uint64_t i = 2; i <= value / 2; i++) {
+      if (value % i == 0) {
        return false;  
-      } 
-    } 
-  } 
+      }
+    }
+  }
   return true;  
-} 
+}
 
-uint64_t nPrime(uint64_t n) { 
+uint64_t nPrime(uint64_t n) {
   uint64_t res = 0;
   uint64_t num = 0;
-  for (uint64_t i = 2 ; num < n; i++) { 
-    if (checkPrime(i)) { 
+  for (uint64_t i = 2 ; num < n; i++) {
+    if (checkPrime(i)) {
       res = i;
       num++;
     }
@@ -29,19 +29,19 @@ uint64_t nPrime(uint64_t n) {
   return res;
 }
 
-uint64_t nextPrime(uint64_t value) { 
+uint64_t nextPrime(uint64_t value) {
   uint64_t next = value + 1;
-  while (!checkPrime(next)) { 
+  while (!checkPrime(next)) {
     next++;
-  } 
+  }
   return next;
-} 
+}
 
-uint64_t sumPrime(uint64_t hbound) { 
+uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (uint64_t a = 2; a < hbound; i++) { 
-    if (checkPrime(a)) { 
+  for (uint64_t a = 2; a < hbound; a++) {
+    if (checkPrime(a)) {
       sum += a;
-    } 
-  } 
-} 
+    }
+  }
+}
