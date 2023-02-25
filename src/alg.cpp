ef768a14dd41c20ead92c044d2e18a5d0de2d0a1
RomanychevStepan <124895851+RomanychevStepan@users.noreply.github.com>
2023-02-25 15:10:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,14 @@
 
 bool checkPrime(uint64_t value) {
   if (value <= 1) {
-    return false; 
+    return false;
   }
-  else {
-    for (uint64_t i = 2; i <= value / 2; i++) {
-      if (value % i == 0) {
-       return false;  
-      }
-    }
-  }
-  return true;  
+   for (uint64_t i = 2; i <= value / 2; i++) {
+     if (value % i == 0) {
+      return false;
+     }
+   }
+   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -43,5 +41,6 @@ uint64_t sumPrime(uint64_t hbound) {
     if (checkPrime(a)) {
       sum += a;
     }
+  return sum
   }
 }
